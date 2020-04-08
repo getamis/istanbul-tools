20322dbf13e7678c592905c681fc3a1846b6675d
Trung Nguyen <24930+trung@users.noreply.github.com>
2020-04-08 17:17:31 -0400

    replace travis with github actions (#17)
    * replace travis with github actions
    * add support extract address from public key

--- cmd/istanbul/address/cmd.go
+++ cmd/istanbul/address/cmd.go
@@ -1,8 +1,11 @@
 package address
 
 import (
+	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+
+	"github.com/ethereum/go-ethereum/p2p/enode"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/urfave/cli"
@@ -15,20 +18,32 @@ var (
 		Usage:  "Extract validator address",
 		Flags: []cli.Flag{
 			nodeKeyHexFlag,
+			nodeIdHexFlag,
 		},
 		Description: `Extract validator address`,
 	}
 )
 
 func address(ctx *cli.Context) error {
-	if !ctx.IsSet(nodeKeyHexFlag.Name) {
-		return cli.NewExitError("Must supply nodekey hex", 10)
+	if (!ctx.IsSet(nodeKeyHexFlag.Name) && !ctx.IsSet(nodeIdHexFlag.Name)) || (ctx.IsSet(nodeKeyHexFlag.Name) && ctx.IsSet(nodeIdHexFlag.Name)) {
+		return cli.NewExitError("Must supply nodekey or nodeid hex", 10)
 	}
-	nodeKeyHex := ctx.String(nodeKeyHexFlag.Name)
-	privKey, err := crypto.HexToECDSA(nodeKeyHex)
-	if err != nil {
-		return err
+	var publicKey ecdsa.PublicKey
+	if ctx.IsSet(nodeKeyHexFlag.Name) {
+		nodeKeyHex := ctx.String(nodeKeyHexFlag.Name)
+		privKey, err := crypto.HexToECDSA(nodeKeyHex)
+		if err != nil {
+			return err
+		}
+		publicKey = privKey.PublicKey
 	}
-	fmt.Printf("0x%s\n", hex.EncodeToString(crypto.PubkeyToAddress(privKey.PublicKey).Bytes()))
+	if ctx.IsSet(nodeIdHexFlag.Name) {
+		pubKey, err := enode.HexPubkey(ctx.String(nodeIdHexFlag.Name))
+		if err != nil {
+			return err
+		}
+		publicKey = *pubKey
+	}
+	fmt.Printf("0x%s\n", hex.EncodeToString(crypto.PubkeyToAddress(publicKey).Bytes()))
 	return nil
 }
--- cmd/istanbul/address/flags.go
+++ cmd/istanbul/address/flags.go
@@ -9,4 +9,8 @@ var (
 		Name:  "nodekeyhex",
 		Usage: "Node key as hex",
 	}
+	nodeIdHexFlag = cli.StringFlag{
+		Name:  "nodeidhex",
+		Usage: "Public key as hex, usually used as enode address",
+	}
 )
